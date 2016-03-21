867567afba1761dd51f6d316cd2f1fa695898fb9
gilderjw <gilderjw@rose-hulman.edu>
2016-03-21 12:27:38 -0400

    makeCardGetName fails

--- src/test/java/devops/hw1/core/BackendTest.java
+++ src/test/java/devops/hw1/core/BackendTest.java
@@ -1,12 +1,19 @@
 package devops.hw1.core;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class BackendTest {
 
 	@Test
+	public void makeCardGetName(){
+		Card c = new Card("Storm crow");
+		assertEquals(c.getName(), "Storm crow");
+	}
+
+	@Test
 	public void testMakeCardWithName() {
 		Card c = new Card("Storm Crow");
 	}
-
 }
