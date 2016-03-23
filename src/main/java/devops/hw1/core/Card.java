b1cabb0dc95ac76466209bd4bb5432aecc9dc35c
gilderjw <gilderjw@rose-hulman.edu>
2016-03-23 16:33:32 -0400

    added javadocs to card and zone class

--- src/main/java/devops/hw1/core/Card.java
+++ src/main/java/devops/hw1/core/Card.java
@@ -3,10 +3,18 @@ package devops.hw1.core;
 public class Card {
 	String name;
 
+	/**
+	 * Constructs a card object with the given name
+	 * @param s name of the card
+	 */
 	public Card(String s){
 		this.name = s;
 	}
 
+	/**
+	 *
+	 * @return the name of the card
+	 */
 	public String getName(){
 		return this.name;
 	}
--- src/main/java/devops/hw1/core/Zone.java
+++ src/main/java/devops/hw1/core/Zone.java
@@ -11,10 +11,20 @@ public enum Zone {
 
 	private ArrayList<Card> cards = new ArrayList<Card>();
 
+	/**
+	 * Adds a card to the zone at the given index
+	 * @param c card to add
+	 * @param i index to place card
+	 */
 	public void addCard(Card c, int i){
 		this.cards.add(i, c);
 	}
 
+	/**
+	 * Determines if the zone contains a card with the given name
+	 * @param name name of the card to look for
+	 * @return true if the card is found in the zone, false otherwise
+	 */
 	public Boolean contains(String name) {
 		for (Card c : this.cards){
 			if(c.name.equals(name))
@@ -23,21 +33,36 @@ public enum Zone {
 		return false;
 	}
 
+	/**
+	 * empties all contents from the zone
+	 */
 	public void empty() {
 		this.cards.clear();
 
 	}
 
+	/**
+	 *
+	 * @return a list of cards that are within the zone
+	 */
 	public Card[] getCards(){
 
 		return this.cards.toArray(new Card[0]);
 	}
 
+	/**
+	 *
+	 * @return the number of cards in the zone
+	 */
 	public int getSize() {
 		return this.cards.size();
 	}
 
 
+	/**
+	 * Removes the card at the given index from the zone
+	 * @param i index of card to remove
+	 */
 	public void remove(int i) {
 		this.cards.remove(i);
 
