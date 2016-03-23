bad9be59745fd94056f8fae5514984d24933cf5a
gilderjw <gilderjw@rose-hulman.edu>
2016-03-23 11:41:34 -0400

    testGetManyZoneContents

--- src/test/java/devops/hw1/core/BackendTest.java
+++ src/test/java/devops/hw1/core/BackendTest.java
@@ -18,6 +18,23 @@ public class BackendTest {
 	}
 
 	@Test
+	public void testGetManyZoneContents(){
+		Backend bknd = new Backend();
+		Card c = new Card("Storm crow");
+		bknd.addCard(Zone.HAND, c, 0);
+
+		Card c1 = new Card("island");
+		bknd.addCard(Zone.HAND, c1, 1);
+
+		Card c2 = new Card("Scornful egotist");
+		bknd.addCard(Zone.HAND, c2, 2);
+
+		assertEquals(c, (bknd.getZoneContents(Zone.HAND)[0]));
+		assertEquals(c1, (bknd.getZoneContents(Zone.HAND)[1]));
+		assertEquals(c2, (bknd.getZoneContents(Zone.HAND)[2]));
+	}
+
+	@Test
 	public void testGetZoneContents(){
 		Backend bknd = new Backend();
 		Card c = new Card("Storm crow");
