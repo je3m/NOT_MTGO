36691daeb7b8c5676376619c0ddf73633e8e6e5d
chris <christopher.malinosky@gmail.com>
2016-03-22 23:39:17 -0400

    GUI Test 1 Succeeded.

--- src/main/java/devops/hw1/core/MTGDuelDecks.java
+++ src/main/java/devops/hw1/core/MTGDuelDecks.java
@@ -2,11 +2,22 @@ package devops.hw1.core;
 
 import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
 public class MTGDuelDecks {
 	private static final Dimension JFRAME_SIZE = new Dimension(1200, 800);
 	
 	
 	public static void main(String[] args){
-
+		JFrame MTGJFrame = setUpJFrame();
+	}
+	
+	public static JFrame setUpJFrame(){
+		JFrame Frame = new JFrame();
+		Frame.setSize(JFRAME_SIZE);
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setVisible(true);
+		Frame.setTitle("MTG Duel Decks");
+		return Frame;
 	}
 }
