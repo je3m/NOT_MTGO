c4e203957ef24bb6446fcf11ad0c00be3aac7270
chris <christopher.malinosky@gmail.com>
2016-03-22 23:28:48 -0400

    First GUI test fails

--- src/main/java/devops/hw1/core/MTGDuelDecks.java
+++ src/main/java/devops/hw1/core/MTGDuelDecks.java
@@ -1,6 +1,11 @@
 package devops.hw1.core;
 
+import java.awt.Dimension;
+
 public class MTGDuelDecks {
+	private static final Dimension JFRAME_SIZE = new Dimension(1200, 800);
+	
+	
 	public static void main(String[] args){
 
 	}
