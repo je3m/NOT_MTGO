6ac3f8e1ad69e685231ac7ccfc9901fa04f131b5
gilderjw <gilderjw@rose-hulman.edu>
2016-03-23 16:15:03 -0400

    TestContains fails

--- src/test/java/devops/hw1/core/ZoneTest.java
+++ src/test/java/devops/hw1/core/ZoneTest.java
@@ -1,6 +1,7 @@
 package devops.hw1.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,6 +19,36 @@ public class ZoneTest {
 		assertEquals(Zone.GRAVEYARD.getSize(), 0);
 	}
 
+	@Test
+	public void testContains(){
+		Zone.BATTLE_FIELD.empty();
+		String[] names = {"Storm Crow",
+				"Scornful Egotist", "One with Nothing", "Sorrow's Path",
+		"Fleshmad Steed"};
+
+		for (int i = 0; i < 5; i++){
+			Zone.BATTLE_FIELD.addCard(new Card(names[i]), i);
+		}
+
+		for (int i = 0; i < 5; i++){
+			assertTrue(Zone.BATTLE_FIELD.contains(names[i]));
+		}
+	}
+
+
+	//	@Test
+	//	public void testRemoveIndexFromZone(){
+	//		Zone.BATTLE_FIELD.empty();
+	//
+	//		for (int i = 0; i < 5; i++){
+	//			Zone.BATTLE_FIELD.addCard(new Card("Storm Crow"), i);
+	//		}
+	//
+	//		assertEquals(Zone.BATTLE_FIELD.getSize(), 5);
+	//
+	//		Zone.BATTLE_FIELD.remove(0);
+	//		assertEquals(Zone.BATTLE_FIELD.getSize(), 4);
+	//	}
 
 	@Test
 	public void testGetZoneSize(){
