7718fd94a79130e08bb81c2aaa14f9698a717c43
Stellan72 <salisbss@rose-hulman.edu>
2016-04-01 09:28:21 -0400

    testSetWindowX fails

--- src/test/java/devops/hw1/core/MTGComponentTest.java
+++ src/test/java/devops/hw1/core/MTGComponentTest.java
@@ -25,5 +25,17 @@ public class MTGComponentTest {
 		
 		
 	}
+	
+	@Test
+	public void testSetWindowX() {
+		MTGComponent comp = new MTGComponent(10, 10);
+		
+		try {
+			comp.setWindowX(-1);
+			fail("Expected IllegalArgumentException");
+		} catch(IllegalArgumentException e) {
+			assertEquals(e.getMessage(), "MTGComponent: -1 is not a valid window width");
+		}
+	}
 
 }
