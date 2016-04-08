8434774bd54b3f962dbd366642dae67f441a06a8
chris <christopher.malinosky@gmail.com>
2016-04-08 08:19:55 -0400

    testMana4 fails.

--- src/test/java/devops/hw1/core/ManaPoolTest.java
+++ src/test/java/devops/hw1/core/ManaPoolTest.java
@@ -21,4 +21,10 @@ public class ManaPoolTest {
 		ManaPool.RED1.add(2);
 		assertEquals(ManaPool.RED1.getAmount(), 4);
 	}
+	
+	@Test
+	public void testMana4(){
+		ManaPool.RED1.remove(2);
+		assertEquals(ManaPool.RED1.getAmount(), 2);
+	}
 }
