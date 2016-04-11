4850d30fd6a3f0649bf464cd28278150ae4d8686
chris <christopher.malinosky@gmail.com>
2016-04-11 10:42:42 -0400

    Improved ManaPoolTests

--- src/test/java/devops/hw1/core/ManaPoolTest.java
+++ src/test/java/devops/hw1/core/ManaPoolTest.java
@@ -36,10 +36,20 @@ public class ManaPoolTest {
 	
 	@Test
 	public void testMana6(){
-		ManaPool.RED1.add(2);
-		ManaPool.RED2.add(2);
+		for(ManaPool m: ManaPool.values()){
+			m.add(1);
+		}
 		ManaPool.emptyMana();
-		assertEquals(ManaPool.RED1.getAmount(), 0);
-		assertEquals(ManaPool.RED2.getAmount(), 0);
+		for(ManaPool m: ManaPool.values()){
+			assertEquals(m.getAmount(), 0);
+		}
+	}
+	
+	@Test
+	public void testMana7(){
+		ManaPool.emptyMana();
+		for(ManaPool m: ManaPool.values()){
+			assertEquals(m.getAmount(), 0);
+		}
 	}
 }
