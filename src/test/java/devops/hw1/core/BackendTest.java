c1b07682802242f27b1f8feb83500569d87a5d29
chris <christopher.malinosky@gmail.com>
2016-04-11 10:24:26 -0400

    testActivateManaAbility11 fails

--- src/test/java/devops/hw1/core/BackendTest.java
+++ src/test/java/devops/hw1/core/BackendTest.java
@@ -1049,6 +1049,22 @@ public class BackendTest {
 		EasyMock.verify(c);
 		ManaPool.COLORLESS1.empty();
 	}
+	
+	@Test
+	public void testActivateManaAbility11(){
+		ManaPool.COLORLESS2.empty();
+		Backend bknd = new Backend();
+		Card c = EasyMock.niceMock(Card.class);
+		EasyMock.expect(c.getManaAbility()).andReturn("T:1");
+		EasyMock.replay(c);
+		
+		c.addManaAbility("T:1");
+		bknd.activateManaAbility(c, false);
+		
+		assertEquals(ManaPool.COLORLESS2.getAmount(), 1);
+		EasyMock.verify(c);
+		ManaPool.COLORLESS2.empty();
+	}
 
 	@Test
 	public void testAdd(){
