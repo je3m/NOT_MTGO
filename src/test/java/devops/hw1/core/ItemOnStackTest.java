93c065bb27291f703bdd2301e57404224666a670
chris <christopher.malinosky@gmail.com>
2016-04-11 10:37:32 -0400

    Added test to ItemOnStack and removed irrelevant methods.

---
+++ src/test/java/devops/hw1/core/ItemOnStackTest.java
@@ -0,0 +1,32 @@
+package devops.hw1.core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+public class ItemOnStackTest {
+	@Test
+	public void testGetCardAndPlayer1(){
+		Card c = EasyMock.niceMock(Card.class);
+		ItemOnStack item = new ItemOnStack(c,false);
+		assertEquals(false,item.getPlayer());
+		assertEquals(c, item.getCard());
+	}
+	
+	@Test
+	public void testGetCardAndPlayer2(){
+		Card c = EasyMock.niceMock(Card.class);
+		ItemOnStack item = new ItemOnStack(c,true);
+		assertEquals(true,item.getPlayer());
+		assertEquals(c, item.getCard());
+	}
+	
+	@Test
+	public void testGetCardAndPlayer3(){
+		Card c = EasyMock.niceMock(Card.class);
+		ItemOnStack item = new ItemOnStack(null,null);
+		assertEquals(null,item.getPlayer());
+		assertEquals(null, item.getCard());
+	}
+}
