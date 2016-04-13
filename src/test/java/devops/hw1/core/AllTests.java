684af6a38715c61ba095552255ccbbd4110fdb3e
dumbnut7410 <gilderjw@rose-hulman.edu>
2016-04-13 18:42:06 -0400

    added new junit suite

---
+++ src/test/java/devops/hw1/core/AllTests.java
@@ -0,0 +1,19 @@
+package devops.hw1.core;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({AbstractDataTest.class,
+				BackendTest.class,
+				DispGUICardTest.class,
+				ItemOnStackTest.class,
+				ManaPoolTest.class,
+				MTGComponentTest.class,
+				NumericDataTest.class,
+				TestCard.class,
+				ZoneTest.class})
+public class AllTests {
+
+}
