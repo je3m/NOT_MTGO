e6e1c9923ed3257791520885159149950eb45183
dumbnut7410 <gilderjw@rose-hulman.edu>
2016-04-13 20:46:37 -0400

    testGetCost passes

--- src/main/java/devops/hw1/core/SMELParser.java
+++ src/main/java/devops/hw1/core/SMELParser.java
@@ -2,13 +2,19 @@ package devops.hw1.core;
 
 public class SMELParser {
 
+	String cost;
+	
 	public SMELParser(String smel) {
+		int start = smel.indexOf('{') + 1;
+		int end = smel.indexOf('}');
+		this.cost = (String) smel.subSequence(start, end);
 		
+
 	}
 
 	public String getCost() {
 		
-		return "7U";
+		return this.cost;
 	}
 
 }
