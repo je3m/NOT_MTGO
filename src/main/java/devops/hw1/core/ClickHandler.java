45c1813997db08d0c5a8927ea6d8e2594c6f54dd
gilderjw <gilderjw@rose-hulman.edu>
2016-04-21 19:02:16 -0400

    can now cast spells

--- src/main/java/devops/hw1/core/ClickHandler.java
+++ src/main/java/devops/hw1/core/ClickHandler.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
  */
 public class ClickHandler implements MouseListener {
 	MTGComponent MTGComp;
-	
-	
+
+
 	/**
 	 * Creates a clickhandler object for a given component
 	 * @param MTGComp the component where the clicks will occur
@@ -20,10 +20,10 @@ public class ClickHandler implements MouseListener {
 	public ClickHandler(MTGComponent MTGComp) {
 		this.MTGComp = MTGComp;
 	}
-	
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		handleClickCard(e);
+		this.handleClickCard(e);
 	}
 
 	/**
@@ -31,61 +31,68 @@ public class ClickHandler implements MouseListener {
 	 * @param e the click event
 	 */
 	public void handleClickCard(MouseEvent e){
-		checkZoneForCardClick(MTGComp.getHandGUICards1(), Zone.HAND);
-		checkZoneForCardClick(MTGComp.getHandGUICards2(), Zone.HAND1);
-		checkZoneForCardClick(MTGComp.getBattleGUICards1(), Zone.BATTLE_FIELD);
-		checkZoneForCardClick(MTGComp.getBattleGUICards2(), Zone.BATTLE_FIELD1);
-		checkDispCardClick1();
-		checkDispCardClick2();
-		checkPassButtonClick1();
-		checkPassButtonClick2();
-		
+		this.checkZoneForCardClick(this.MTGComp.getHandGUICards1(), Zone.HAND);
+		this.checkZoneForCardClick(this.MTGComp.getHandGUICards2(), Zone.HAND1);
+		this.checkZoneForCardClick(this.MTGComp.getBattleGUICards1(), Zone.BATTLE_FIELD);
+		this.checkZoneForCardClick(this.MTGComp.getBattleGUICards2(), Zone.BATTLE_FIELD1);
+		this.checkDispCardClick1();
+		this.checkDispCardClick2();
+		this.checkPassButtonClick1();
+		this.checkPassButtonClick2();
+
 	}
 
 	/**
 	 * Handles click checking for the first players GUI card
 	 */
 	private void checkDispCardClick1() {
-		if(MTGComp.getDispGUICard1() != null){
-			if(MTGComp.getDispGUICard1().getRec().contains(MTGComp.getMousePosition())){
-				MTGComp.setDispGUICard1(null);
-				MTGComp.repaint();
+		DispGUICard gCard = this.MTGComp.getDispGUICard1();
+
+		if(this.MTGComp.getDispGUICard1() != null){
+			if(this.MTGComp.getDispGUICard1().getRec().contains(this.MTGComp.getMousePosition())){
+				this.MTGComp.setDispGUICard1(null);
+				this.MTGComp.repaint();
 			} else {
-				for(int i = 0; i < MTGComp.getDispGUICard1().getAbilityBoxes().length; i++){
-					if(MTGComp.getDispGUICard1().getAbilityBoxes()[i].contains(MTGComp.getMousePosition())){
-						if(MTGComp.getDispGUICard1().getCard().getManaAbility() != null && i==0) {
-							MTGComp.getBackend().activateManaAbility(MTGComp.getDispGUICard1().getCard(), true);
-						} else {
-							Backend.activateAbility(MTGComp.getDispGUICard1().getCard(), MTGComp.getDispGUICard1().getZone(), MTGComp.getDispGUICard1().getIndex(), i);
+				if((gCard.getAbilityBoxes().length > 1) && gCard.getAbilityBoxes()[1].contains(this.MTGComp.getMousePosition())){
+					Backend.getInstance().castSpell(gCard.getZone(), gCard.getCard(), gCard.index, true, null, null);
+				}else {
+
+					for(int i = 0; i < (gCard.getAbilityBoxes().length); i++){
+						if(gCard.getAbilityBoxes()[i].contains(this.MTGComp.getMousePosition())){
+							if((gCard.getCard().getManaAbility() != null) && (i==0)) {
+								this.MTGComp.getBackend().activateManaAbility(gCard.getCard(), true);
+							} else {
+								Backend.activateAbility(gCard.getCard(), gCard.getZone(), gCard.getIndex(), i);
+							}
+
+							this.MTGComp.setDispGUICard1(null);
+							this.MTGComp.repaint();
+							break;
 						}
-						
-						MTGComp.setDispGUICard1(null);
-						MTGComp.repaint();
-						break;
 					}
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Handles click checking for the second players GUI card
 	 */
 	private void checkDispCardClick2() {
-		if(MTGComp.getDispGUICard2() != null){
-			if(MTGComp.getDispGUICard2().getRec().contains(MTGComp.getMousePosition())){
-				MTGComp.setDispGUICard2(null);
-				MTGComp.repaint();
+		if(this.MTGComp.getDispGUICard2() != null){
+			if(this.MTGComp.getDispGUICard2().getRec().contains(this.MTGComp.getMousePosition())){
+				this.MTGComp.setDispGUICard2(null);
+				this.MTGComp.repaint();
 			} else {
-				for(int i = 0; i < MTGComp.getDispGUICard2().getAbilityBoxes().length; i++){
-					if(MTGComp.getDispGUICard2().getAbilityBoxes()[i].contains(MTGComp.getMousePosition())){
-						if(MTGComp.getDispGUICard2().getCard().getManaAbility() != null && i==0) {
-							MTGComp.getBackend().activateManaAbility(MTGComp.getDispGUICard2().getCard(), false);
+				for(int i = 0; i < this.MTGComp.getDispGUICard2().getAbilityBoxes().length; i++){
+					if(this.MTGComp.getDispGUICard2().getAbilityBoxes()[i].contains(this.MTGComp.getMousePosition())){
+						if((this.MTGComp.getDispGUICard2().getCard().getManaAbility() != null) && (i==0)) {
+							this.MTGComp.getBackend().activateManaAbility(this.MTGComp.getDispGUICard2().getCard(), false);
 						} else {
-							Backend.activateAbility(MTGComp.getDispGUICard2().getCard(), MTGComp.getDispGUICard2().getZone(), MTGComp.getDispGUICard2().getIndex(), i);
+							Backend.activateAbility(this.MTGComp.getDispGUICard2().getCard(), this.MTGComp.getDispGUICard2().getZone(), this.MTGComp.getDispGUICard2().getIndex(), i);
 						}
-						MTGComp.setDispGUICard2(null);
-						MTGComp.repaint();
+						this.MTGComp.setDispGUICard2(null);
+						this.MTGComp.repaint();
 						break;
 					}
 				}
@@ -94,45 +101,45 @@ public class ClickHandler implements MouseListener {
 	}
 
 	/**
-	 * Checks to see if any card in an array list is clicked and tells the backend if something is clicked 
+	 * Checks to see if any card in an array list is clicked and tells the backend if something is clicked
 	 * @param e the click event
 	 * @param cardAL the array list containing all cards to be checked
 	 * @param start starting zone of the cards in the arraylist
 	 */
 	public void checkZoneForCardClick(ArrayList<GUICard> cardAL, Zone start){
 		for(int i = 0; i < cardAL.size(); i++){
-			if(cardAL.get(i).getRec().contains(MTGComp.getMousePosition())){
-				if((start == Zone.HAND || start == Zone.BATTLE_FIELD) && MTGComp.getDispGUICard1() == null){
-					MTGComp.generateDispGUICard1(cardAL.get(i).getCard(), i, start);
-				} else if ((start == Zone.HAND1 || start == Zone.BATTLE_FIELD1) && MTGComp.getDispGUICard2() == null){
-					MTGComp.generateDispGUICard2(cardAL.get(i).getCard(), i, start);
+			if(cardAL.get(i).getRec().contains(this.MTGComp.getMousePosition())){
+				if(((start == Zone.HAND) || (start == Zone.BATTLE_FIELD)) && (this.MTGComp.getDispGUICard1() == null)){
+					this.MTGComp.generateDispGUICard1(cardAL.get(i).getCard(), i, start);
+				} else if (((start == Zone.HAND1) || (start == Zone.BATTLE_FIELD1)) && (this.MTGComp.getDispGUICard2() == null)){
+					this.MTGComp.generateDispGUICard2(cardAL.get(i).getCard(), i, start);
 				}
-				MTGComp.repaint();
+				this.MTGComp.repaint();
 			}
 		}
 	}
-	
+
 	/**
 	 * Checks to see if player 1's pass button is clicked and if so tells the backend to try to pass priority from that player
 	 */
 	public void checkPassButtonClick1() {
-		if(MTGComp.getPassButton1().contains(MTGComp.getMousePosition())) {
-			MTGComp.getBackend().passPriority(true);//TODO make this pass in which player is trying to pass priority
-			MTGComp.repaint();
+		if(this.MTGComp.getPassButton1().contains(this.MTGComp.getMousePosition())) {
+			this.MTGComp.getBackend().passPriority(true);//TODO make this pass in which player is trying to pass priority
+			this.MTGComp.repaint();
 		}
 	}
-	
+
 	/**
 	 * Checks to see if player 2's pass button is clicked and if so tells the backend to try to pass priority from that player
 	 */
 	public void checkPassButtonClick2() {
-		if(MTGComp.getPassButton2().contains(MTGComp.getMousePosition())) {
+		if(this.MTGComp.getPassButton2().contains(this.MTGComp.getMousePosition())) {
 
-			MTGComp.getBackend().passPriority(false);//TODO make this pass in which player is trying to pass priority
-			MTGComp.repaint();
+			this.MTGComp.getBackend().passPriority(false);//TODO make this pass in which player is trying to pass priority
+			this.MTGComp.repaint();
 		}
 	}
-	
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
