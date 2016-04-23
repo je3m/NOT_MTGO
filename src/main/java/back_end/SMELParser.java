0f2ddc9845790d082128290ee8ab3c391524e966
gilderjw <gilderjw@rose-hulman.edu>
2016-04-23 11:27:44 -0400

    testCostEffect passes

--- src/main/java/back_end/SMELParser.java
+++ src/main/java/back_end/SMELParser.java
@@ -2,24 +2,42 @@ package back_end;
 
 public class SMELParser {
 
-	String cost;
-	
+	private String cost;
+	public String effect;
+
 	public SMELParser(String smel) {
-		if (smel == "" || smel == null)
-			return;
-		
-		int start = smel.indexOf('{') + 1;
-		int end = smel.indexOf('}');
-		this.cost = (String) smel.subSequence(start, end);
-		
+		if (smel == null)
+			throw new IllegalArgumentException("SMEL: no cost");
+
+		smel = smel.replaceAll("\\s+", "");
+
+		String[] arr = smel.split("(?:(\\{|\\}))");
+
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i].equals("COST")){
+				this.cost = arr[i+1];
+				i++;
+			} else if (arr[i].equals("EFFECT")){
+				this.effect = arr[i+1];
+				i++;
+			}
+		}
+
+
+
 
 	}
 
 	public String getCost() {
 		if (this.cost == null)
 			throw new RuntimeException("SMEL: no cost");
-		
+
 		return this.cost;
+	}
+
+	public String getEffect() {
+		// TODO Auto-generated method stub
+		return this.effect;
 	}
 
 }
