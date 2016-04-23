0528412cabba7d069879bf2aaa870734862f0041
gilderjw <gilderjw@rose-hulman.edu>
2016-04-23 11:27:37 -0400

    testCostEffect fails

--- src/test/java/devops/hw1/core/SMELParserTest.java
+++ src/test/java/devops/hw1/core/SMELParserTest.java
@@ -1,6 +1,7 @@
 package devops.hw1.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -12,34 +13,43 @@ public class SMELParserTest {
 	public void testGetCost() {
 		String smel = "COST {7U}";
 		SMELParser parse = new SMELParser(smel);
-		
+
 		assertEquals(parse.getCost(), "7U");
-		
+
 		smel = "COST {B}";
 		parse = new SMELParser(smel);
-		
+
 		assertEquals("B", parse.getCost());
-		
+
 		smel = "";
 		parse = new SMELParser(smel);
-		
+
 		try{
 			parse.getCost();
 			fail("expected exception");
 		} catch (RuntimeException e){
 			assertEquals(e.getMessage(), "SMEL: no cost");
 		}
-		
-		parse = new SMELParser(null);
+
+
 		try{
-			parse.getCost();
+			parse = new SMELParser(null);
 			fail("expected exception");
 		} catch (RuntimeException e){
 			assertEquals(e.getMessage(), "SMEL: no cost");
 		}
-		
-		
-		
+	}
+
+	@Test
+	public void testCostEfectParse(){
+		String LlanowarElves = "COST { TAP } EFFECT { MANA G}";
+		SMELParser parse = new SMELParser(LlanowarElves);
+
+		assertEquals("TAP", parse.getCost());
+		assertEquals("MANAG", parse.getEffect());
+
+
+
 	}
 
 }
