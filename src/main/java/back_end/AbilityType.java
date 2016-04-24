4bfc50d3840cf7977d248ca3d4a9578b0b4242ca
gilderjw <gilderjw@rose-hulman.edu>
2016-04-24 12:51:26 -0400

    getTypeFromString passes

---
+++ src/main/java/back_end/AbilityType.java
@@ -0,0 +1,24 @@
+package back_end;
+
+/**
+ * Represents the types of abilities that a card can have
+ * @author jim
+ */
+public enum AbilityType {
+	ACTIVATED,
+	TRIGGERED,
+	STATIC,
+	ETB,
+	MANA;
+
+	public static AbilityType getTypeFromString(String s){
+		//TODO: invalid input
+		for (AbilityType z: AbilityType.values()){
+			if (s.equals(z.name())){
+				return z;
+			}
+		}
+		return null;
+
+	}
+}
