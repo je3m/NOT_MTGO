d40be8ef460019c02248417032dfd070c3803ec0
gilderjw <gilderjw@rose-hulman.edu>
2016-05-05 21:41:03 -0400

    testActivateManaAbility passes again

--- src/main/java/back_end/Backend.java
+++ src/main/java/back_end/Backend.java
@@ -145,10 +145,10 @@ public class Backend {
 	public void activateAbility(Card c, Zone z, int i, int abInd, Card target, Boolean targetPlayer, Zone targetZone) {
 		Ability a =	c.getAbilities()[abInd];
 
-		if(!c.isFlash() &&
+		if((a.getType() != AbilityType.MANA)&& (!c.isFlash() &&
 				((this.turn != Zone.getPlayerFromZone(z)) || (!this.stack.isEmpty()) ||
 						((this.phase != Phase.FIRST_MAIN1) && (this.phase != Phase.SECOND_MAIN1) &&
-								(this.phase != Phase.FIRST_MAIN2) && (this.phase != Phase.SECOND_MAIN2)))){
+								(this.phase != Phase.FIRST_MAIN2) && (this.phase != Phase.SECOND_MAIN2))))){
 			throw new RuntimeException("cannot cast spell at this time");
 		}
 		if((target != null) && !canTarget(c, z, target, targetZone)) {
