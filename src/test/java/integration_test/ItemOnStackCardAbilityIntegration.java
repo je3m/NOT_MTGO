5ce4381a68dd20348de509e6a4a13fc956cef1e1
gilderjw <gilderjw@rose-hulman.edu>
2016-05-11 18:57:38 -0400

    ItemOnStackCardAbilityIntegration.IntegrateWtihCard passes

--- src/test/java/integration_test/ItemOnStackCardAbilityIntegration.java
+++ src/test/java/integration_test/ItemOnStackCardAbilityIntegration.java
@@ -1,6 +1,7 @@
 package integration_test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
@@ -34,6 +35,24 @@ public class ItemOnStackCardAbilityIntegration {
 		assertEquals("CAST", item.getAbility().getText());
 
 
+	}
+
+	@Test
+	public void IntegrateWtihCard(){
+		Card c = new Card("Arbor Elf", "G", "G", "Creature- Elf Druid", "T:G",
+				new ArrayList<String>(), 1, 1, MTGDuelDecks.LLANOWAR_ELVES_PATH, false);
+
+		ItemOnStack item = new ItemOnStack(c, null, true, null, false, null);
+
+		assertTrue(item.getCard().getName().equals("Arbor Elf"));
+		assertTrue(item.getCard().getCost().equals("G"));
+		assertTrue(item.getCard().getColor().equals("G"));
+		assertTrue(item.getCard().getType().equals("Creature- Elf Druid"));
+		assertEquals(item.getCard().getManaAbility(), "T:G");
+		assertEquals(item.getCard().getAbilities().length, 0);
+		assertEquals(item.getCard().getPower(),1);
+		assertEquals(item.getCard().getToughness(),1);
+		assertTrue(item.getCard().getImage().equals(MTGDuelDecks.LLANOWAR_ELVES_PATH));
 	}
 
 }
